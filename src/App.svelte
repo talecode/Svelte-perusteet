f414567812c84db8da324619cda0b661b82cdd22
talecode <ville-leppanen@outlook.com>
2019-11-15 13:39:35 +0200

    filut gitiin

---
+++ src/App.svelte
@@ -0,0 +1,82 @@
+<script>
+  import Book from "./Book.svelte";
+
+  let title = "";
+  let price = "";
+  let description = "";
+
+  let books = [];
+
+  function sellBook(event) {
+    const newBook = event.detail;
+    console.log(newBook);
+
+    books = [...books, newBook];
+    console.log(books);
+
+    title = "";
+    price = "";
+    description = "";
+  }
+
+  // TEHTÄVÄ
+  let buyedBooks = [];
+
+  $: total = "";
+
+  function buyBook(event) {}
+  // TEHTÄVÄ
+</script>
+
+<style>
+  section {
+    width: 30rem;
+    margin: auto;
+  }
+  input {
+    width: 100%;
+  }
+</style>
+
+<section>
+  <h1>Sell your book!</h1>
+  <input type="text" id="title" placeholder="Title" bind:value={title} />
+  <input type="number" id="price" placeholder="Price" bind:value={price} />
+  <input
+    type="text"
+    id="title"
+    placeholder="Description"
+    bind:value={description} />
+  <Book
+    bookTitle={title}
+    bookPrice={price}
+    bookDescription={description}
+    buttonText="Sell"
+    on:submitBook={sellBook} />
+</section>
+
+<hr />
+
+<section>
+  <h1>Books in sale!</h1>
+
+  {#if books.length === 0}
+    <p>No books</p>
+  {:else}
+    {#each books as book}
+      <Book
+        bookTitle={book.bookTitle}
+        bookPrice={book.bookPrice}
+        bookDescription={book.bookDescription}
+        buttonText="Buy"
+        on:submitBook={buyBook} />
+    {/each}
+  {/if}
+</section>
+
+<!-- TEHTÄVÄ ALLE -->
+<hr />
+
+<section>
+  <h1>Buyed books!</h1>
+</section>
---
+++ src/Book.svelte
@@ -0,0 +1,54 @@
+<script>
+  export let bookTitle;
+  export let bookPrice;
+  export let bookDescription;
+  export let buttonText;
+
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  function submitBook() {
+    dispatch("submitBook", {
+      bookTitle,
+      bookPrice,
+      bookDescription
+    });
+  }
+</script>
+
+<style>
+  div {
+    margin: 1rem;
+    box-shadow: 0 2px 8px black;
+    padding: 1rem;
+    border-radius: 5px;
+    background: white;
+  }
+
+  button {
+    font: inherit;
+    padding: 0.15rem 0.5rem;
+    background: purple;
+    border: 1px solid purple;
+    color: white;
+    cursor: pointer;
+  }
+
+  button:hover,
+  button:active {
+    box-shadow: 1px 1px 6px black;
+  }
+</style>
+
+{#if bookTitle || bookPrice || bookDescription}
+
+  <div>
+    <h1>{bookTitle}</h1>
+    <h2>{bookPrice}</h2>
+    <p>{bookDescription}</p>
+    {#if bookTitle && bookPrice && bookDescription}
+      <button on:click={submitBook}>{buttonText}</button>
+    {/if}
+  </div>
+{/if}
